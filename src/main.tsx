7844d240a685972dc2914959e175adb67b4ba6ae
AiFlowTools <aiflowtools@gmail.com>
2025-06-16 20:30:50 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import App from './App'
 import './index.css'   // ← this line is critical so Tailwind/base styles load
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
